348c37f1fea801c8f696f7752b5d4fb6b8cbb0ee
Barathi Shankar <barathishankarr@gmail.com>
2025-09-09 21:23:49 +0530

    Update Police_thief.py
    Small update for constructor development

--- Police_thief.py
+++ Police_thief.py
@@ -1,7 +1,18 @@
 #Program to impliment the classic police thief game in system usign python
 class game :
+  def__init(pn,n)
+    player_name=pn;
+    Role=n;
   def read():
   def disp():
   def finding():
 def main()
-  game
+  pname,name;
+  p1 = game(pname,role);
+  p2 = game(pname,role);
+  p3 = game(pname,role);
+  p4 = game(pname,role);
+  g.read();
+  g.disp();
+  g.finding();
+#UNDER DEVELOPMENT
